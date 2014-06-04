7234ea40d18bcbb2452498edd8a2c3dbc8e6002f
llhed <llhed_lm@hotmail.com>
2014-06-04 14:01:34 +0800

    aa

---
+++ DataServer/src/com/anran/pojo/PositionCard.java
@@ -0,0 +1,38 @@
+package com.anran.pojo;
+
+public class PositionCard {
+
+	private String cardNum;
+	private String cardStatus;
+	private String ipAdress;
+	private String timeStamp;
+	
+	
+	
+	public String getCardNum() {
+		return cardNum;
+	}
+	public void setCardNum(String cardNum) {
+		this.cardNum = cardNum;
+	}
+	public String getCardStatus() {
+		return cardStatus;
+	}
+	public void setCardStatus(String cardStatus) {
+		this.cardStatus = cardStatus;
+	}
+	public String getIpAdress() {
+		return ipAdress;
+	}
+	public void setIpAdress(String ipAdress) {
+		this.ipAdress = ipAdress;
+	}
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
+}
