fad5155725649c3a9d63328269135ec3a1563572
llhed <llhed_lm@hotmail.com>
2014-07-07 10:38:14 +0800

    1

---
+++ DataServer/src/com/anran/pojo/PostionStationPojo.java
@@ -0,0 +1,22 @@
+package com.anran.pojo;
+
+public class PostionStationPojo {
+
+	private String ipAddress;
+	private String status;
+	
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
